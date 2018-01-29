ae45e6baa4558db5908f17408eee50f40acb55d8
Bob Hamelers <Apple@wcw-stud-145-109-54-57.wireless.uva.nl>
2018-01-29 20:54:54 +0100

    Fix errors and comment in LoginViewController and SignUpViewController

--- CountryInfo/Controllers/LoginViewController.swift
+++ CountryInfo/Controllers/LoginViewController.swift
@@ -2,6 +2,11 @@
 //  LoginViewController.swift
 //  CountryInfo
 //
+//  This Controller takes care of the login of an user.
+//  It displays a GIF File.
+//  With email and password an user can login when the user tapped the login button.
+//  Next to that, an user can signup if an user tapped the signup button.
+//
 //  Created by Bob Hamelers on 11/01/2018.
 //  Copyright © 2018 Bob Hamelers. All rights reserved.
 //
@@ -11,73 +16,68 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    // MARK: Constants
+    // MARK: Properties.
     let loginSeague = "LoginSeague"
     
-    // MARK: Outlets
+    // MARK: Outlets.
     @IBOutlet weak var textFieldLoginEmail: UITextField!
     @IBOutlet weak var textFieldLoginPassword: UITextField!
     @IBOutlet weak var GifView: UIImageView!
 
-    // MARK: Actions
+    // MARK: Actions.
     
     @IBAction func loginDidTouch(_ sender: AnyObject) {
         Auth.auth().signIn(withEmail: textFieldLoginEmail.text!,
                            password: textFieldLoginPassword.text!, completion: { (user, error) in if(error != nil){
                             self.createAlert(title: "FAILURE", message: "Your E-mail, Password or both are incorrect or not known")
                             }})
-        // Login Authentication
+        // MARK: Login authentication when e-mail and password a correct and filled in.
     }
     
-    // MARK: Function, DismissKeyboard
+    /// MARK: Function, dismiss keyboard.
     @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
         textFieldLoginEmail.resignFirstResponder()
         textFieldLoginPassword.resignFirstResponder()
     }
     
-    // MARK: Functions
+    /// MARK: ViewDidLoad standard.
     override func viewDidLoad() {
         super.viewDidLoad()
         GifView.loadGif(name: "world")
-        // Load Loginview
+        // MARK: Load GIF file.
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
         self.view.addGestureRecognizer(tapGesture)
+        // MARK: Recognizer for DismissKeyboard.
         
         Auth.auth().addStateDidChangeListener() { auth, user in
             if user != nil {
                 self.performSegue(withIdentifier: self.loginSeague, sender: nil)
             }
         }
-        // Recognizer for DismissKeyboard and User Authentication
+        // MARK: Recognizer for user authentication.
     }
     
+    /// MARK: Create alert function that displays a popup.
     func createAlert (title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.default, handler: { (action) in
             alert.dismiss(animated: true, completion: nil)
         }))
         self.present(alert, animated: true, completion: nil)
-        // Alert function for different alerts in SignUp and Login
+        // MARK: Alert function with title (Okay) and message for different alerts.
     }
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender:
-//        Any?) {
-//        if segue.identifier == "SignUpSegue" {
-//            let signUpViewController = segue.destination as!
-//            SignUpViewController
-//        }
-//        // Prepare for SignUpViewController
-//    }
 
+    /// MARK: Function that handles memory warnings.
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // MARK: Dispose of any resources that can be recreated.
     }
 }
 
 extension LoginViewController: UITextFieldDelegate {
     
+    /// MARK: Return textfields.
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == textFieldLoginEmail {
             textFieldLoginPassword.becomeFirstResponder()
@@ -87,5 +87,5 @@ extension LoginViewController: UITextFieldDelegate {
         }
         return true
     }
-    // Return function in extension to return information in textfields
+    // MARK: Return function in extension to return information in textfields.
 }
--- CountryInfo/Controllers/SignUpViewController.swift
+++ CountryInfo/Controllers/SignUpViewController.swift
@@ -2,6 +2,10 @@
 //  SignUpViewController.swift
 //  CountryInfo
 //
+//  This Controller takes care of the signup of an user.
+//  With email and password (and repeat password) an user can signup when the user tapped the signup button.
+//  Next to that, an user can click on cancel.
+//
 //  Created by Bob Hamelers on 11/01/2018.
 //  Copyright © 2018 Bob Hamelers. All rights reserved.
 //
@@ -12,39 +16,36 @@ import UIKit
 
 class SignUpViewController: UIViewController {
     
-    // MARK: Constants
+    // MARK: Properties.
     let signUpSeague = "SignUpSeague"
-    let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_"
     var ref: DatabaseReference!
     
-    // MARK: Outlets
+    // MARK: Outlets.
     @IBOutlet weak var textFieldSignUpEmail: UITextField!
-    @IBOutlet weak var textFieldSignUpUsername: UITextField!
     @IBOutlet weak var textFieldSignUpPassword: UITextField!
     @IBOutlet weak var textFieldRepeatPassword: UITextField!
-    // MARK: Actions
 
+    // MARK: Actions.
     @IBAction func signUpAndLogin(_ sender: AnyObject) {
         
         if textFieldSignUpEmail.text!.contains("@") == false {
             self.createAlert(title: "FAILURE", message: "You insert an invalid e-mail.")
+            // MARK: A check for false e-mailadresses.
             return
         }
-//        if textFieldSignUpUsername.text!.contains() ==
-//
-//        textFieldSignUpUsername.text!.cointains(charactersIn: ACCEPTABLE_CHARACTERS)
         
         if textFieldSignUpPassword.text! != textFieldRepeatPassword.text! {
             self.createAlert(title: "FAILURE", message: "Your password and repeat password are not the same.")
+            // MARK: A check for not-known and fake passwords.
             return
         }
         
         if textFieldSignUpPassword.text!.count < 6 {
             self.createAlert(title: "FAILURE", message: "Your password has to be 6 characters or longer.")
+            // MARK: A check that a password has to be longer then 6 characters.
             return
         }
         
-        // SignUp Authentication
         Auth.auth().createUser(withEmail: textFieldSignUpEmail.text!,
                                password: textFieldSignUpPassword.text!) { user, error in
                                 if error == nil {
@@ -53,56 +54,52 @@ class SignUpViewController: UIViewController {
                                 }
                                 else {
                                     self.createAlert(title: "FAILURE", message: "This e-mail is already used by a user")
+                                    // MARK: A check for already know e-mail.
                                     return
                                 }
         }
-//        // SignUp Database
-//        let user = Auth.auth().currentUser
-//        if let user = user {
-//            let userID: String = user.uid
-//            let userEmail: String = self.textFieldSignUpEmail.text!
-//            let userPassword: String = self.textFieldSignUpPassword.text!
-//            //        let userUsername: String = self.textFieldSignUpUsername.text!
-//
-//            self.ref.child("Users").child(userID).setValue(["Email": userEmail, "Password": userPassword])
-//        }
+        // MARK: SignUp authentication with e-mail and password.
     }
     
-    // MARK: Function, DismissKeyboard
+    /// MARK: Function, dismiss keyboard.
     @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
         textFieldSignUpEmail.resignFirstResponder()
-        textFieldSignUpUsername.resignFirstResponder()
         textFieldSignUpPassword.resignFirstResponder()
         textFieldRepeatPassword.resignFirstResponder()
     }
     
-    // MARK: Functions
+    /// MARK: ViewDidLoad standard.
     override func viewDidLoad() {
         super.viewDidLoad()
         
         ref = Database.database().reference()
+        // MARK: Database reference.
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
         self.view.addGestureRecognizer(tapGesture)
+        // MARK: Recognizer for DismissKeyboard.
         
         Auth.auth().addStateDidChangeListener() { auth, user in
             if user != nil {
                 self.performSegue(withIdentifier: self.signUpSeague, sender: nil)
             }
         }
-        // Recognizer for DismissKeyboard and User Authentication
+        // MARK: Recognizer for user authentication.
     }
     
+    /// MARK: Create alert function that displays a popup.
     func createAlert (title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.default, handler: { (action) in
             alert.dismiss(animated: true, completion: nil)
         }))
         self.present(alert, animated: true, completion: nil)
-        // Alert function for different alerts in SignUp and Login
+        // MARK: Alert function with title (Okay) and message for different alerts.
     }
 
+    /// MARK: Function that handles memory warnings.
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // MARK: Dispose of any resources that can be recreated.
     }
 }
